3e83a0b69ed2390677ae9ebcaaca93c2d76bdee4
tchungnz <tchung.nz@gmail.com>
2016-07-05 16:56:19 +0100

    Exercise 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,15 @@
 class DockingStation
+
+attr_accessor :number_of_bikes
+
+def initialize
+  @number_of_bikes = 0
+end
+
   def release_bike
     Bike.new
+  end
+  def dock_bike
+
   end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -8,5 +8,7 @@ it 'release_bike method should create new instance of Bike class' do
   expect(DockingStation.new.release_bike).to be_instance_of Bike
   expect(DockingStation.new.release_bike.working?).to eq true
 end
-
+it 'should respond to a method called dock_bike' do
+  expect(DockingStation.new).to respond_to(:dock_bike)
+end
 end
